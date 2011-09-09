04ecccfdbe04fbe4d70b1b1c1b0433f998d94801
Eric Muyser <eric@muyser.com>
2011-09-09 15:11:48 -0700

    fix sort order.

--- classes/testimonial_actions.php
+++ classes/testimonial_actions.php
@@ -2,7 +2,7 @@
 
 	class Testimonial_Actions extends Cms_ActionScope {
 		public function statements() {
-			$this->data['statements'] = Testimonial_Statement::create()->where('is_enabled=1')->find_all();
+			$this->data['statements'] = Testimonial_Statement::create()->where('is_enabled=1')->order('sort_order')->find_all();
 		}
 	}
 	
