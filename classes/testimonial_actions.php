5e079368f2ce924e7dc5e5a70c305761f3e6222e
Chris Snyder <chris@keyedupmedia.com>
2013-07-09 16:47:02 -0400

    Add action so you can have a "featured testimonials" page.

--- classes/testimonial_actions.php
+++ classes/testimonial_actions.php
@@ -4,5 +4,8 @@
 		public function statements() {
 			$this->data['statements'] = Testimonial_Statement::create()->where('is_enabled=1')->order('sort_order')->find_all();
 		}
+
+		public function featured_statements() {
+			$this->data['statements'] = Testimonial_Statement::create()->where('is_enabled=1 and featured=1')->order('sort_order')->find_all();
+		}
 	}
-	
